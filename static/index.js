410d7d2559f96f62f0cc46ec587fe24fc62196fe
Alaz Sengul <alazsengul@gmail.com>
2020-12-20 18:27:07 -0500

    progress bar update

--- static/index.js
+++ static/index.js
@@ -54,6 +54,12 @@ var fetch_poems = function() {
 
     $("#progress_bar").css({ width: "5%" });
     $("#progress_bar").animate({ width: "15%" });
+    setTimeout(
+        function() { $("#progress_bar").animate({ width: "30%" }); },
+    1000);
+    setTimeout(
+        function() { $("#progress_bar").animate({ width: "75%" }); },
+    3000);
 
     $.ajax({
 
